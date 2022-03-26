7060eadd516e27ac79e4c1faea991dd7ffaa1ba6
Jonathan Perry <jonathanperry@sequencemedia.net>
2022-03-26 19:01:40 +0000

    `use`

--- app.mjs
+++ app.mjs
@@ -1,7 +1,5 @@
 #!/usr/bin/env node
 
-import 'dotenv/config'
-
 import debug from 'debug'
 
 import {
--- src/index.mjs
+++ src/index.mjs
@@ -6,24 +6,35 @@ import normalise from '#watch-cache/normalise'
 
 const log = debug('@sequencemedia/watch-cache')
 const error = debug('@sequencemedia/watch-cache:error')
-const info = debug('@sequencemedia/watch-cache:info')
 
 const CACHE = '.cache'
 
 const cacheSet = new Set()
 
+function use (key, f = () => {}) {
+  const l = debug(`@sequencemedia/watch-cache:${key}`)
+
+  return function log (v) {
+    let r
+    try {
+      r = f(v)
+      l(v)
+    } catch (e) {
+      handleError(e)
+    }
+
+    return r
+  }
+}
+
 function handleAdd (filePath) {
   log('handleAdd')
-
-  info(filePath)
 
   cacheSet.add(filePath)
 }
 
 function handleUnlink (filePath) {
   log('handleUnlink')
-
-  info(filePath)
 
   cacheSet.delete(filePath)
 }
@@ -45,8 +56,9 @@ export default function watchCache (cache = CACHE) {
 
   return (
     chokidar.watch(normalise(cache))
-      .on('add', handleAdd)
-      .on('unlink', handleUnlink)
+      .on('change', use('handle-change'))
+      .on('add', use('handle-add', handleAdd))
+      .on('unlink', use('handle-unlink', handleUnlink))
       .on('error', handleError)
   )
 }
